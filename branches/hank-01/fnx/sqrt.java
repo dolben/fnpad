f8ebea8c16e6c367b644f59b09602a861ff6b32b
hdolben <hdolben@ac58d126-fd34-11de-b5bc-ffd2847a4b57>
2010-01-09 18:50:02 +0000

    added missing class to license reference

--- branches/hank-01/fnx/sqrt.java
+++ branches/hank-01/fnx/sqrt.java
@@ -1,7 +1,7 @@
 /**
  *
  *  fnPad: a functional programming style calculator
- *  Copyright (c) 2001-2010 Hank Dolben, org.dolben.fn.LICENSE
+ *  Copyright (c) 2001-2010 Hank Dolben, org.dolben.fn.Pad.LICENSE
  */
 package fnx;
 
